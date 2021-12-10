30d03fc68adb833af6f42da1acfae686aa210fb3
kdabhi7 <73013243+kdabhi7@users.noreply.github.com>
2021-12-10 01:37:24 -0500

    Updated code with calculations for time difference
    Added code that finds accurate time differences from raw data csv

--- app/src/main/java/com/example/recorddata/RollingResistance.java
+++ app/src/main/java/com/example/recorddata/RollingResistance.java
@@ -5,32 +5,34 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Time;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.List;
-import java.util.Scanner;
 
 public class RollingResistance {
     private static final String COMMA_DELIMITER = ",";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         List<List<String>> records = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Pablo\\Desktop\\M2\\CsvTest\\src\\Tiles_trialtest.csv"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\kushd\\Documents\\cs4605\\Code\\Carpet_trial4 - Carpet_trial4.csv"))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(COMMA_DELIMITER);
                 records.add(Arrays.asList(values));
             }
         }
-        String[] timeStamps = new String[records.size() - 27];
-        Double[] xVal = new Double[records.size() - 27];
-        Double[] yVal = new Double[records.size() - 27];
-        Double[] zVal = new Double[records.size() - 27];
-        for (int i = 27; i < records.size(); i++) {
-            timeStamps[i - 27] = records.get(i).get(0);
-            xVal[i - 27] = Double.parseDouble(records.get(i).get(1));
-            yVal[i - 27] = Double.parseDouble(records.get(i).get(2));
-            zVal[i - 27] = Double.parseDouble(records.get(i).get(3));
+        SimpleDateFormat[] timeStamps = new SimpleDateFormat[records.size()];
+        Double[] xVal = new Double[records.size()];
+        Double[] yVal = new Double[records.size()];
+        Double[] zVal = new Double[records.size()];
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy h:mm:ss", Locale.ENGLISH);
+        Date[] dateStamp = new Date[records.size()];
+        for (int i = 0; i < records.size(); i++) {
+            dateStamp[i] = sdf.parse(records.get(i).get(0));
+            xVal[i] = Double.parseDouble(records.get(i).get(1));
+            yVal[i] = Double.parseDouble(records.get(i).get(2));
+            zVal[i] = Double.parseDouble(records.get(i).get(3));
         }
 //        System.out.println(Arrays.toString(xVal));
         // get these from user input
@@ -44,18 +46,60 @@ public class RollingResistance {
         smoothArray(yVal, 2);
         smoothArray(zVal, 2);
 
-        DrawGraph xGraph = new DrawGraph(Arrays.asList(xVal));
-        xGraph.setPreferredSize(new Dimension(2000, 600));
-        JFrame frame = new JFrame("DrawGraph");
+        //for finding magnitude
+        Double[] magnitude = new Double[xVal.length];
+        for (int i = 0; i < xVal.length; i++) {
+            Double mag = (xVal[i] * xVal[i]) + (yVal[i] * yVal[i]) + (zVal[i] * zVal[i]);
+            magnitude[i] = Math.sqrt(mag);
+        }
+
+        smoothArray(magnitude, 2);
+
+        //for graphing magnitude
+        DrawGraph magGraph = new DrawGraph(Arrays.asList(magnitude));
+        magGraph.setPreferredSize(new Dimension(2000, 600));
+        JFrame frame = new JFrame("Magnitude Graph");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(xGraph);
+        frame.getContentPane().add(magGraph);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        //for finding velocity
+
+        //finding accurate time difference
+        Double counter = 1.0;
+        int i = 1;
+        Double[] timeDiff = new Double[dateStamp.length - 1];
+        while (i < dateStamp.length) {
+            int j = i;
+            while ((dateStamp[j].getTime() - dateStamp[j - 1].getTime() == 0) && j < dateStamp.length-1) {
+                counter++;
+                j++;
+                //System.out.print(j);
+            }
+//            if(counter != 1.0) {
+//                System.out.println((1 / counter));
+//            }
+
+
+            if (counter == 1.0) {
+                timeDiff[i - 1] = 1.0;
+            }
+
+            for (int k = i; k <= j && counter != 1.0; k++) {
+                timeDiff[k - 1] = (1 / counter);
+            }
+            i = j + 1;
+            counter = 1.0;
+        }
+        //System.out.print(Arrays.toString(timeDiff));
+        System.out.println(dateStamp[1].getTime() - dateStamp[0].getTime());
+        System.out.println(Arrays.toString(timeDiff));
     }
 
     // http://phrogz.net/js/framerate-independent-low-pass-filter.html
-    public static void smoothArray(Double[] values, int smoothing){
+    public static void smoothArray(Double[] values, int smoothing) {
         Double value = values[0]; // start with the first input
         for (int i=1; i < values.length; i++){
             Double currentValue = values[i];
